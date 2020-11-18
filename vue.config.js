d1c241b54daee5965b3ebc257a30c5ea5f591662
yjx199908 <1399590856@qq.com>
2020-11-18 19:15:38 +0800

    config the origin project

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+module.exports = {
+  lintOnSave: true,
+  publicPath:'./',
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('assets',resolve('src/assets'))
+      .set('views',resolve('src/views'))
+      .set('css',resolve('src/css'))
+      .set('js',resolve('src/js'))
+      .set('components',resolve('src/components'))
+  }
+};
