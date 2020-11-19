61435374ae50ae6cd501f2b4847de1921c8f63a9
liao19990119 <1321270039@qq.com>
2020-11-19 20:01:07 +0800

    mmm

--- src/views/music.vue
+++ src/views/music.vue
@@ -1,32 +1,35 @@
 <template>
-  <div class='music'>
-      <h1>music</h1>
-      
+  <div class="music">
+    <h1 class="title-h1">music</h1>
+    <div class="music-box">
+      <!-- <div class="music-menubar">音乐菜单</div> -->
+      <input type="text" class="music-seek" />
+      <button div="music-btn" @click="btnriff">搜索</button>
+      <div class="music-log"></div>
+      <div class="min-box">
+        <div class="minlog1"></div>
+        <div class="minlog2"></div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-
-  export default {
-    name:'',
-    props:{
-      Abc:String
-    },
-    data(){
-        return {
-
-        }
-    },
-    methods:{
-        
-    },
-    computed:{
-
-    },
-    components:{
-
-    }
-  }
+export default {
+  name: "",
+  props: {
+    Abc: String,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    //音乐搜索按鈕
+    btnriff: function () {},
+  },
+  computed: {},
+  components: {},
+};
 </script>
 <style scoped>
-  @import '~css/music.css';
+@import "~css/music.css";
 </style>
