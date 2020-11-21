124030f116ff8dcbd13972ed66e79fa2b5014a65
liao19990119 <1321270039@qq.com>
2020-11-21 14:39:33 +0800

    music

--- src/views/music.vue
+++ src/views/music.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="music">
     <h1 class="title-h1">music</h1>
+    
+    <audio controls @timeupdate="timeUpdate">
+    <source src="../assets/music/临安初雨.mp3" type="audio/mpeg">
+    </audio>
     <div class="music-box">
       <div class="music-bar" @mouseenter="mousdrop" @mouseleave="mousadd"></div>
      <div :class='[mubarShow,mubarstop]'>
      <ul class="music-bar-text">
+       <!-- 注意注意 -->
        <li>lailll</li>
      </ul>
      </div>
@@ -17,11 +22,18 @@
       </div> 
       <div class="progress-bar">
       <div class="music-porgre"></div>
+      
       <span><img src="../assets/music/last.png" class="music-btn-last"></span>
-      <span><img src="../assets/music/play.png" class="music-btn-play"></span>
+      <span id="musicPause">
+        <img src="../assets/music/play.png" class="music-btn-position music-btn-play" id="music-play">
+      </span>
+      <span id="musicPlay">
+        <img src="../assets/music/pause.png" class="music-btn-position music-btn-pause" id="music-pause">
+      </span>
       <span><img src="../assets/music/last.png" class="music-btn-next"></span>
+        <!-- <span><img src="../assets/music/音量.png"></span> -->
       <div class="music-porgre-border">
-         <span class="music-porgre-solid"></span>
+         <span class="music-porgre-solid" :style="{'left':-100+((currentMusicTime / currentDuration) * 100) + '%'}"></span>
         </div>
       
       </div>
@@ -39,34 +51,48 @@ export default {
     return { 
         mubarShow:'',
         mubarstop:'',
+        currentMusicTime:0,
+        currentDuration:1000000
     };
   },
   methods: {
-    hd: function(){
-       this.mubarShow='';
-       this.mubarstop='',
-      console.log(1)
-    },
     mousdrop:function(event){
      
        this.mubarShow='showbar';
-       this.mubarstop='musicbartext',
-      console.log(2)
-
+       this.mubarstop='musicbartext'
     },
     mousadd:function(event){
-      this.mubarShow='';
-      console.log(1)
-      
+      this.mubarShow=''
+    
     },
- 
+    timeUpdate(e){
+      let srcEle = e.srcElement;
+      this.currentMusicTime = srcEle.currentTime
+      this.currentDuration = srcEle.duration
+      console.log("已更改：")
+      console.log(this.currentMusicTime,this.currentDuration)
+    }
   },
   computed: {},
   components: {},
   mounted() {
-    setTimeout(()=>{
-        console.log("音乐时长：",document.getElementById("audio").duration);
-      },1000)
+    // setTimeout(()=>{
+    //     console.log("音乐时长：",document.getElementById("audio").duration);
+    //   },1000);
+      setTimeout(()=>{
+        let musicpause=document.getElementById("music-play");
+        let musicplay=document.getElementById("music-pause");
+        let Play=document.getElementById("musicPlay");
+        let Pause= document.getElementById("musicPause");
+        Play.onclick=function(){
+          musicplay.style.display="none"
+          musicpause.style.display="block"
+        }
+        Pause.onclick=function(){
+          musicplay.style.display="block"
+          musicpause.style.display="none"
+        }
+      },1000);
   },
 };
 </script>
