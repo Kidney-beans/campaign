fcdb87cfbd960f66a5c1b80c961c10d56fd111ee
yjx199908 <1399590856@qq.com>
2020-11-22 15:13:23 +0800

    aaa

--- src/views/music.vue
+++ src/views/music.vue
@@ -2,12 +2,12 @@
   <div class="music">
     <h1 class="title-h1">music</h1>
     
-    <audio controls @timeupdate="timeUpdate">
+    <audio controls @timeupdate="timeUpdate" id="audio" >
     <source src="../assets/music/临安初雨.mp3" type="audio/mpeg">
     </audio>
     <div class="music-box">
-      <div class="music-bar" @mouseenter="mousdrop" @mouseleave="mousadd"></div>
-     <div :class='[mubarShow,mubarstop]'>
+      <div class="music-bar"></div>
+     <div class="showbar musicbartext">
      <ul class="music-bar-text">
        <!-- 注意注意 -->
        <li>lailll</li>
@@ -23,19 +23,19 @@
       <div class="progress-bar">
       <div class="music-porgre"></div>
       
-      <span><img src="../assets/music/last.png" class="music-btn-last"></span>
-      <span id="musicPause">
-        <img src="../assets/music/play.png" class="music-btn-position music-btn-play" id="music-play">
+      <span><img src="~assets/music/last.png" class="music-btn-last some-btns"></span>
+      <span id="musicPause" v-show="!pause" @click="musicPause">
+        <img src="~assets/music/pause.png" class="some-btns music-btn-position music-btn-play" id="music-play">
       </span>
-      <span id="musicPlay">
-        <img src="../assets/music/pause.png" class="music-btn-position music-btn-pause" id="music-pause">
+      <span id="musicPlay" v-show="pause" @click="musicPlay">
+        <img src="~assets/music/play.png" class="some-btns music-btn-position music-btn-pause" id="music-pause">
       </span>
-      <span><img src="../assets/music/last.png" class="music-btn-next"></span>
+      <span><img src="../assets/music/last.png" class="some-btns music-btn-next"></span>
 
-      <volumn-control side-length="4vh" top="1.5vh" left="15%" top-offset="-16.5vh" bar-height="134px"></volumn-control>
+      <volumn-control side-length="4vh" top="1.5vh" left="15%" top-offset="-16.5vh" bar-height="134px" @change="changeV"></volumn-control>
         <!-- <span><img src="../assets/music/音量.png"></span> -->
-      <div class="music-porgre-border">
-         <span class="music-porgre-solid" :style="{'left':-100+((currentMusicTime / currentDuration) * 100) + '%'}"></span>
+      <div class="music-porgre-border" @click="adaptMusicTime" real="p">
+         <span class="music-porgre-solid" real="s" :style="{'left':-100+((currentMusicTime / currentDuration) * 100) + '%'}"></span>
         </div>
       
       </div>
@@ -53,52 +53,69 @@ export default {
   },
   data() {
     return { 
-        mubarShow:'',
-        mubarstop:'',
         currentMusicTime:0,
-        currentDuration:1000000
+        currentDuration:1000000,
+        pause:true
     };
   },
   methods: {
-    mousdrop:function(event){
-     
-       this.mubarShow='showbar';
-       this.mubarstop='musicbartext'
-    },
-    mousadd:function(event){
-      this.mubarShow=''
-    
-    },
     timeUpdate(e){
       let srcEle = e.srcElement;
       this.currentMusicTime = srcEle.currentTime
       this.currentDuration = srcEle.duration
       console.log("已更改：")
       console.log(this.currentMusicTime,this.currentDuration)
+    },
+    musicPlay(){
+      document.getElementById('audio').play()
+      this.pause = false
+    },
+    musicPause(){
+      document.getElementById('audio').pause()
+      this.pause = true
+    },
+    adaptMusicTime(e){
+      console.log(e)
+      let real = e.srcElement.getAttribute("real")
+      if(real == 'p'){
+        let positionX = e.offsetX
+        let parentWidth = e.srcElement.offsetWidth
+        console.log("parentWidth:",parentWidth)
+        this.currentMusicTime = (positionX / parentWidth * this.currentDuration)
+        this.musicPlay()
+        this.setMusicTime()
+      }
+      else{
+        let hiddenLength = -(-1 + this.currentMusicTime / this.currentDuration) * e.srcElement.offsetWidth
+        let positionX = e.offsetX-hiddenLength
+        console.log("hiddenLength:",hiddenLength)
+        let parentWidth = e.srcElement.offsetWidth
+        console.log("parentWidth:",parentWidth)
+        this.currentMusicTime = (positionX / parentWidth * this.currentDuration)
+        this.musicPlay()
+        this.setMusicTime()
+      }
+    },
+    setMusicTime(){
+      document.getElementById('audio').currentTime = this.currentMusicTime
+    },
+    changeV(va){
+      if(va < 0.15){
+        va = 0.15
+      }
+      else if(va > 0.95){
+        va = 0.95
+      }
+      document.getElementById('audio').volume = va
     }
+
   },
   computed: {},
   components: {
     VolumnControl
   },
   mounted() {
-    // setTimeout(()=>{
-    //     console.log("音乐时长：",document.getElementById("audio").duration);
-    //   },1000);
-      setTimeout(()=>{
-        let musicpause=document.getElementById("music-play");
-        let musicplay=document.getElementById("music-pause");
-        let Play=document.getElementById("musicPlay");
-        let Pause= document.getElementById("musicPause");
-        Play.onclick=function(){
-          musicplay.style.display="none"
-          musicpause.style.display="block"
-        }
-        Pause.onclick=function(){
-          musicplay.style.display="block"
-          musicpause.style.display="none"
-        }
-      },1000);
+    
   },
 };
 </script>
