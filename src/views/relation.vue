98a0e064f859273e0dc0e255f64606aa9aaa5a6f
liao19990119 <1321270039@qq.com>
2020-12-03 23:01:22 +0800

    11

--- src/views/relation.vue
+++ src/views/relation.vue
@@ -2,51 +2,51 @@
   <div id='relation'>
         <h1 class="relation-title-h1">relation</h1>
         <div class="relationBox">
-          <div>
-            <ul :class="{active:isActive,pingLunBox:true}" >
-              <li v-for="item in pingLunArr" :key="item.id">我：{{item.text}}</li>
-            </ul>
-
+          <div :class="{testBox:true,testBoxgo:isActive}">
+              <img src="~assets/relation/closeRightAssessFrame.png" class="close" @click="closeRightAssessFrame">
+              <p v-for="item in community.pingLunArr" :key="item.id">我：{{item.text}}</p>
           </div>
           <div class="contentBox">
               <div class="titleHead">
-                  <div @click="jump(0)" :class="{'active':0==current_index}">社区</div>  
-                  <div @click="jump(1)" :class="{'active':1==current_index}">收到的赞与评论</div>
-                  <div @click="jump(2)" :class="{'active':2==current_index}">发布</div>
+                  <div @click="jump(0)" :class="{'active':0==current_page_index}">社区</div>  
+                  <div @click="jump(1)" :class="{'active':1==current_page_index}">收到的赞与评论</div>
+                  <div @click="jump(2)" :class="{'active':2==current_page_index}">发布</div>
               </div>
               <!-- 内容 -->
               <div class="titleContent" :style="{'top':top_offset}">
               <!-- 世界 -->
                   <div class="myWorld"> 
-                           <ul class="myWorld-Content-box">
-                           <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1606893387282&di=3679662d98c374827f5f3147c22f0a8e&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20180420%2Fa02ae1f573bb430ca298fe5783b4f38c.jpeg">
-                             <li class="contentText">
-                                <p>
-                                    花褪残红青杏小。燕子飞时，绿水人家绕。<br>
-                                    枝上柳绵吹又少。天涯何处无芳草。 <br>
-                                    墙里秋千墙外道。墙外行人，墙里佳人笑。<br>
-                                    笑渐不闻声渐悄。多情却被无情恼。<br>
-                                </p>
-                             </li>
-                             <span class="myWorld-Content-ico">
-                                  <img src="../assets/relation/pinglun.png" @click="pingLun">
-                                  <img src="../assets/relation/zan.png" @click="dianZan"><p>{{contentZan}}</p>
-                             </span>
-                             <span class="iptBox" v-show="inputShow">
-                                    <input type="text" v-model="pingLunValue">
-                                     <button @click="send">评论</button>
-                             </span>                      
-                           </ul>
+                           <div class="myWorld-Content-box" v-for="(item,index) in community.newsArr" :key="item.imgUrl">
+                           <img :src="item.imgUrl">
+                             <div class="contentText">
+                                <p>{{item.text}}</p>
+                             </div>
+                             <div class="myWorld-Content-ico">
+                                  <img src="~assets/relation/pinglun.png" @click="pingLun(item,index)">
+                                  <img src="~assets/relation/zan.png" @click="dianZan(item)"><p>{{item.applaudCount}}</p>
+                             </div>
+                             <div class="iptBox" v-show="index == community.currentTargetNewsIndex">
+                                    <input type="text" v-model="item.currentAssessInput">
+                                     <button @click="send(item)">评论</button>
+                             </div>                      
+                           </div>
                   </div>
                          <!-- 收到的赞 -->
                   <div class="getPraise">
+                    <div>
+                          <div>
+                                <img src="">
+                                <p></p>
+                          </div>
+                    
+                    </div>
                       
                   </div>
                   <!-- 发布 -->
                   <div class="release">
                         <div class="releaseHead">
-                        <p>我的发布</p>
-                        <button>发布</button>
+                        <p class="release-button">我的发布</p>
+                        <button @click="releaseButton">发布</button>
                         
                         </div>
                   <div class="releaseBox">
@@ -54,21 +54,22 @@
                         <el-input
                                   type="textarea"
                                   placeholder="请输入内容"
-                                  v-model="textarea"
+                                  v-model="releaseValue"
                                   maxlength="800"
                                   show-word-limit>
                         </el-input>
-                        <el-upload
-                              action="https://jsonplaceholder.typicode.com/posts/"
-                              list-type="picture-card"
-                              :limit="4"
-                              :on-preview="handlePictureCardPreview"
-                              :on-remove="handleRemove">
-                              <i class="el-icon-plus"></i>
-                            </el-upload>
-                            <el-dialog :visible.sync="dialogVisible">
-                              <img width="100%" :src="dialogImageUrl" alt="">
-                            </el-dialog>
+                        <div class="upload-container">
+                          <div v-for="item in releaseModule.uploadModel.alreadyUploadList" :key="item.url" class="upload-item">
+                            <img :src="item.url"/>
+                          </div>
+                          <div class="imgUpload upload-item" @click="triggerUpload">
+                            <img src="~assets/relation/ad.png">
+                            <input type="file" name="file" id="file" @change="fileChange">
+                          </div>
+          
+                        </div>
+                        
+                        
                       </div>
                   </div>
               </div>
@@ -84,34 +85,46 @@
 
     data() {
       return {
-        dialogImageUrl: '',
-        dialogVisible: false,
-        textarea:"",
-        aa:1,
-        pingLunValue:"",
+        releaseValue:"",
         isActive:false,
-        inputShow:false,
         top_offset:'0vh',
-        current_index:0,
-        contentZan:0,
+        current_page_index:0,
+        community:{
+          newsArr:[
+            {
+              imgUrl:"https://x0.ifengimg.com/res/2020/CCF4D1398A75722E75374EAE06402B66E0061E82_size2419_w1670_h879.png",
+              text:"目前嫦娥五号探测器的着陆器、上升器组合体正在月球表面开展采样探测。北京时间今天4时53分，着陆器、上升器组合体完成了月球钻取采样及封装，表取采样正在按计划进行。",
+              pingLunArr:[],
+              currentAssessInput:'',
+              assessing:false,
+              applaudCount:0
+            },
+              {
+              imgUrl:"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4238902235,685766808&fm=26&gp=0.jpg",
+              text:"花褪残红青杏小。燕子飞时，绿水人家绕。枝上柳绵吹又少。天涯何处无芳草。墙里秋千墙外道。墙外行人，墙里佳人笑。笑渐不闻声渐悄。多情却被无情恼。",
+              pingLunArr:[],
+              currentAssessInput:'',
+              assessing:false,
+              applaudCount:0
+            },
+          ],
+          currentTargetNewsIndex:0,
+          pingLunArr:[
+            {
+              id:0,
+              text:"hello"
+            },
+          ],
 
-        pingLunArr:[
-          {
-            id:0,
-            text:"hello"
+        },
+        receiveApplaud:{
+
+        },
+        releaseModule:{
+          uploadModel:{
+            alreadyUploadList:[]
           },
-        ],
-        newsArr:[
-                {
-                  imgUrl:"https://x0.ifengimg.com/res/2020/CCF4D1398A75722E75374EAE06402B66E0061E82_size2419_w1670_h879.png",
-                  text:"目前嫦娥五号探测器的着陆器、上升器组合体正在月球表面开展采样探测。北京时间今天4时53分，着陆器、上升器组合体完成了月球钻取采样及封装，表取采样正在按计划进行。"
-                },
-                 {
-                  imgUrl:"https://x0.ifengimg.com/res/2020/CCF4D1398A75722E75374EAE06402B66E0061E82_size2419_w1670_h879.png",
-                  text:"目前嫦娥五号探测器的着陆器、上升器组合体正在月球表面开展采样探测。北京时间今天4时53分，着陆器、上升器组合体完成了月球钻取采样及封装，表取采样正在按计划进行。"
-                },
-        
-        ]
+        }
       };
     },
     props:{
@@ -125,54 +138,53 @@
         console.log(key, keyPath);
       },
       jump(index){
-        this.current_index = index
+        this.current_page_index = index
         this.top_offset = `${-index * 100}vh `
       },
-      dianZan(){
-          this.contentZan++
-         
+      dianZan(item){
+          item.applaudCount++
       },
-      pingLun(){
-        this.inputShow=!this.inputShow
-        this.isActive=!this.isActive
-      
+      pingLun(item,index){
+        this.isActive = true
+        this.community.pingLunArr = item.pingLunArr
+        item.assessing = !item.assessing
+        this.community.currentTargetNewsIndex = index
       },
-      send(){
-        if(this.pingLunValue==""){
+      send(a){
+        if(a.currentAssessInput==""){
 
         }else{
-          
-        // this.aa=this.pingLunValue
-        this.pingLunArr.id=this.pingLunArr.length
-       
         let obj = {}
-        obj.text = this.pingLunValue
-        this.pingLunArr.push(obj)
-        console.log(this.pingLunArr)
-        this.pingLunValue=""
+        obj.text = a.currentAssessInput
+        this.community.pingLunArr.push(obj)
+        a.currentAssessInput=""
         }
-
     },
-      getdate() {
-            let date = new Date();
-            this.year = date.getFullYear();
-            this.month = date.getMonth() + 1;
-            this.day = date.getDate();
-            this.h = date.getHours();
-            this.m = date.getMinutes();
-            this.s = date.getSeconds();
-      },
-       handleRemove(file, fileList) {
-        console.log(file, fileList);
-      },
-      handlePictureCardPreview(file) {
-        this.dialogImageUrl = file.url;
-        this.dialogVisible = true;
-      }
-      
+      triggerUpload(){
+        document.getElementById("file").click()
 
+      },
+      fileChange(event){
+        console.log(event.srcElement.files[0])
+        let file = event.srcElement.files[0]
+        let temp_url = URL.createObjectURL(file)
+        console.log(temp_url)
+        this.releaseModule.uploadModel.alreadyUploadList.push({
+          url:temp_url
+        })
+      },
+      releaseButton(event){
+            this.releaseValue
+             console.log( this.releaseValue)
+      },
+      closeRightAssessFrame(){
+        this.isActive = false
+      }
     },
     computed:{
+    },
+    mounted(){
+
     }
   }
 </script>
